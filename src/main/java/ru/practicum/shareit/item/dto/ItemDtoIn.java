c1ab47b8eb62050ffe5a8dead9de4766cea3aeb0
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2022-09-09 15:41:31 +0300

    Correct ItemDtoIn and ItemMapper.

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoIn.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoIn.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-//@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 public class ItemDtoIn {
